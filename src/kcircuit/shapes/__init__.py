81e0425b121ded9068d782e8d82524ffe291f5e6
jsmatias <smatias.jean@gmail.com>
2025-06-23 11:15:03 +0200

    Clean up

---
+++ src/kcircuit/shapes/__init__.py
@@ -0,0 +1,5 @@
+from ._vector import Vector #type:ignore
+from .circle import Circle #type:ignore
+from .rectangle import Rectangle #type:ignore
+from .shape import Shape #type:ignore
+from .taper import Taper #type:ignore
